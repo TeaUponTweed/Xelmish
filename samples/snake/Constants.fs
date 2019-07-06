dbd3b9048393fe9c0ffbd4a63a07bd747199ef3d
Michael Mason <TeaUponTweed@gmail.com>
2019-07-05 22:47:00 -0600

    update screen resolution

--- samples/snake/Constants.fs
+++ samples/snake/Constants.fs
@@ -2,11 +2,9 @@
 
 open Xelmish.Model
 
-let highScoreFile = "./highscore.txt"
-
 let gridWidth = 64
 let gridHeight = 64
 let padding = 0
 let tiledim = 8
-let resWidth = padding + (tiledim * gridWidth) + padding + (tiledim * 6) + padding
-let resHeight = padding + (tiledim * gridHeight) + padding
+let resWidth = tiledim * gridWidth
+let resHeight = tiledim * gridHeight
